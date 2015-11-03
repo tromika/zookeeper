f51ebb7524529f501da09f086d6bed9cc9cd5780
Tamas Szuromi <tromika@gmail.com>
2015-11-03 12:16:57 +0100

    Ports changed

--- 3.4.6/ubuntu/14.04/entrypoint.sh
+++ 3.4.6/ubuntu/14.04/entrypoint.sh
@@ -13,7 +13,7 @@ EOF
 
 # server.1=...
 if [ -n "$SERVERS" ]; then
-    python -c "print '\n'.join(['server.%i=%s:2888:3888' % (i + 1, x) for i, x in enumerate('$SERVERS'.split(','))])" >> /opt/zookeeper/conf/zoo.cfg
+    python -c "print '\n'.join(['server.%i=%s:2889:3889' % (i + 1, x) for i, x in enumerate('$SERVERS'.split(','))])" >> /opt/zookeeper/conf/zoo.cfg
 fi
 
 exec "$@"
